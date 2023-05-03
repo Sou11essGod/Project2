db5ba752a394cbfbfa838329dc15b7a21623a3e7
Sou11essGod <98413857+Sou11essGod@users.noreply.github.com>
2023-05-03 18:33:48 -0400

    Add files via upload

---
+++ TaxEastCoast.py
@@ -0,0 +1,152 @@
+item_price_NJ = float(input("Enter the price for the item: "))
+if item_price_NJ > 5:
+   tax_NJ = item_price_NJ % 6.625
+else:
+   tax_NJ = 0
+print("")
+print("The tax in New Jersy is:", tax_NJ)
+print("The item with tax in NJ is: ",item_price_NJ+tax_NJ)
+
+item_price_NY = float(input("Enter the price for the item: "))
+if item_price_NY > 5:
+   tax_NY = item_price_NY % 4.875
+else:
+   tax_NY = 0
+print("")
+print("The tax in New York is:", tax_NY)
+print("The item with tax in NY is: ",item_price_NY+tax_NY)
+
+item_price_PA = float(input("Enter the price for the item: "))
+if item_price_PA > 5:
+   tax_PA = item_price_PA % 3.07
+else:
+   tax_PA = 0
+print("")
+print("The tax in Pennsylvania is:", tax_PA)
+print("The item with tax in PA is: ",item_price_PA+tax_PA)
+
+item_price_OH = float(input("Enter the price for the item: "))
+if item_price_OH > 5:
+   tax_OH = item_price_OH % 3.99
+else:
+   tax_OH = 0
+print("")
+print("The tax in Ohio is:", tax_OH)
+print("The item with tax in OH is: ",item_price_OH+tax_OH)
+
+item_price_MA = float(input("Enter the price for the item: "))
+if item_price_MA > 5:
+   tax_MA = item_price_MA % 6.25
+else:
+   tax_MA = 0
+print("")
+print("The tax in Massachusetts is:", tax_MA)
+print("The item with tax in MA is: ",item_price_MA+tax_MA)
+
+item_price_VT = float(input("Enter the price for the item: "))
+if item_price_VT > 5:
+   tax_VT = item_price_VT % 6.625
+else:
+   tax_VT = 0
+print("")
+print("The tax in Vermont is:", tax_VT)
+print("The item with tax in VT is: ",item_price_VT+tax_VT)
+
+item_price_CT = float(input("Enter the price for the item: "))
+if item_price_CT > 5:
+   tax_CT = item_price_CT % 6.35
+else:
+   tax_CT = 0
+print("")
+print("The tax in Conneticut is:", tax_CT)
+print("The item with tax in CT is: ",item_price_CT+tax_CT)
+
+item_price_RI = float(input("Enter the price for the item: "))
+if item_price_RI > 5:
+   tax_RI = item_price_RI % 7.00
+else:
+   tax_RI = 0
+print("")
+print("The tax in Rhode Island is:", tax_RI)
+print("The item with tax in RI is: ",item_price_RI+tax_RI)
+
+item_price_WV = float(input("Enter the price for the item: "))
+if item_price_WV > 5:
+   tax_WV = item_price_WV % 6.00
+else:
+   tax_WV = 0
+print("")
+print("The tax in West Virginia is:", tax_WV)
+print("The item with tax in WV is: ",item_price_WV+tax_WV)
+
+item_price_NC = float(input("Enter the price for the item: "))
+if item_price_NC > 5:
+   tax_NC = item_price_NC % 4.75
+else:
+   tax_NC = 0
+print("")
+print("The tax in North Carolina is:", tax_NC)
+print("The item with tax in NC is: ",item_price_NC+tax_NC)
+
+item_price_DE = float(input("Enter the price for the item: "))
+if item_price_DE > 5:
+   tax_DE = item_price_DE % 0
+else:
+   tax_DE = 0
+print("")
+print("The tax in Delaware is:", tax_DE)
+print("The item with tax in DE is: ",item_price_DE+tax_DE)
+
+item_price_SC = float(input("Enter the price for the item: "))
+if item_price_SC > 5:
+   tax_SC = item_price_SC % 7.00
+else:
+   tax_SC = 0
+print("")
+print("The tax in South Carolina is:", tax_SC)
+print("The item with tax in SC is: ",item_price_SC+tax_SC)
+
+item_price_GA = float(input("Enter the price for the item: "))
+if item_price_GA > 5:
+   tax_GA = item_price_GA % 4.00
+else:
+   tax_GA = 0
+print("")
+print("The tax in Georgia is:", tax_GA)
+print("The item with tax in GA is: ",item_price_GA+tax_GA)
+
+item_price_FL = float(input("Enter the price for the item: "))
+if item_price_FL > 5:
+   tax_FL = item_price_FL % 6.00
+else:
+   tax_FL = 0
+print("")
+print("The tax in Florida is:", tax_FL)
+print("The item with tax in FL is: ",item_price_FL+tax_FL)
+
+item_price_ME = float(input("Enter the price for the item: "))
+if item_price_ME > 5:
+   tax_ME = item_price_ME % 5.50
+else:
+   tax_ME = 0
+print("")
+print("The tax in Maine is:", tax_ME)
+print("The item with tax in ME is: ",item_price_ME+tax_ME)
+
+item_price_MD = float(input("Enter the price for the item: "))
+if item_price_MD > 5:
+   tax_MD = item_price_MD % 6.00 
+else:
+   tax_MD = 0
+print("")
+print("The tax in Maryland is:", tax_MD)
+print("The item with tax in MD is: ",item_price_MD+tax_MD)
+
+item_price_NH = float(input("Enter the price for the item: "))
+if item_price_NH > 5:
+   tax_NH = item_price_NH % 0.00
+else:
+   tax_NH = 0
+print("")
+print("The tax in New Hampshire is:", tax_NH)
+print("The item with tax in NH is: ",item_price_NH+tax_NH)
